54b8665d2cfc239a5d4122e6872c1403a32348dd
YOOH HAN HWANG <103127767+rokmc1256@users.noreply.github.com>
2023-01-31 03:15:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -29,14 +29,16 @@ while (DoOrder !== '종료') {
         if (myList.length === 0) {
             console.log("삭제하실 내용이 없습니다.");
         }
-        if (myList.length < DoOrder) {
-            console.log("존재하지 않는 인덱스 입니다.");
-        }
-        if (myList.length > 0) {
-            const delNumber = prompt("삭제하실 인덱스 번호를 입력하세요.");
-            const delValue = myList[delNumber];
-            myList.splice(delNumber, 1);
-            console.log(delValue + ' 이(가) 목록에서 삭제되었습니다.');
+        else {
+            const delNumber = parseInt(prompt("삭제하실 인덱스 번호를 입력하세요."));
+            if (myList.length === delNumber) {
+                console.log("존재하지 않는 인덱스 입니다.");
+            }
+            else {
+                const delValue = myList[delNumber];
+                myList.splice(delNumber, 1);
+                console.log(delValue + ' 이(가) 목록에서 삭제되었습니다.');
+            }
         }
     }
 }
